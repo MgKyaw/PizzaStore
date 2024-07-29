908447f2bfe8157c925de87f57057fd468369aee
MgKyaw <mgkyaw20112@gmail.com>
2024-07-29 23:46:13 +0700

    feat: Config Swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Todo API", Description = "keep track of your tasks", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
